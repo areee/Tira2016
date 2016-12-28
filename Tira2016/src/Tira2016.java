e3bfa7e25e01ddbd2734812ed429fd9bf76ca454
areee <areee@users.noreply.github.com>
2016-12-28 08:47:46 +0200

    learning how to use arguments in command line

--- Tira2016/src/Tira2016.java
+++ Tira2016/src/Tira2016.java
@@ -4,7 +4,15 @@
 public class Tira2016 {
 
     public static void main(String[] args) {
+        System.out.println("Moro!");
+        int i = 0;
+        for (String s : args) {
+            System.out.println("arg " + i + ": " + s);
+            i++;
+        }
 
+        // 1.) Lue input.txt args[0]:sta
+        // 2.) Muodosta keko ja 3.) kirjoita output.txt args[1]:een
     }
 
 }
