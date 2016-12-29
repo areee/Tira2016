970bb6443c1aaaa93ab025af349a31e58484fd28
areee <areee@users.noreply.github.com>
2016-12-29 08:30:29 +0200

    modified heap + added comparator

---
+++ Tira2016/src/Comparator.java
@@ -0,0 +1,38 @@
+
+public class Comparator {
+
+    public Comparator() {
+    }
+
+    // Tosi, jos ja vain jos a on pienempi kuin b.
+    public boolean isLessThan(int a, int b) {
+        return a < b;
+    }
+
+    // Tosi, jos ja vain jos a on pienempi tai yhtä suuri kuin b.
+    public boolean isLessThanOrEqual(int a, int b) {
+        return a <= b;
+    }
+
+    // Tosi, jos ja vain jos a on yhtä suuri kuin b.
+    public boolean isEqualTo(int a, int b) {
+        return a == b;
+    }
+
+    // Tosi, jos ja vain jos a on suurempi kuin b.
+    public boolean isGreaterThan(int a, int b) {
+        return a > b;
+    }
+
+    // Tosi, jos ja vain jos a on suurempi tai yhtä suuri kuin b.
+    public boolean isGreaterThanOrEqual(int a, int b) {
+        return a >= b;
+    }
+
+    // Tosi, jos ja vain jos a on vertailtavissa.
+    // Tässä: onko objekti a kokonaisluku (=Integer).
+    public boolean isComparable(Object a) {
+        return (a instanceof Integer); // tarkista, onko ok!
+    }
+
+}
--- Tira2016/src/Heap.java
+++ Tira2016/src/Heap.java
@@ -1,9 +1,72 @@
 
 public class Heap {
 
+    private int size;
+
     public Heap() {
-        
-        
+        size = 0;
     }
 
+    // Palauttaa prioriteettijonon P alkioiden lukumäärän.
+    public int size() {
+        return size;
+    }
+
+    // Testaa onko P tyhjä.
+    public boolean isEmpty() {
+        return size == 0;
+    }
+
+    // Lisää uuden alkion e avaimella k P:hen.
+    public void insertItem(int k, String e) {
+
+    }
+
+    // Palauttaa ja postaa P:stä alkion, jolla on pienin avain. 
+    // Virhe esiintyy prioriteettijonon ollessa tyhjä.
+    public void removeMinElement() {
+        try {
+
+        } catch (Exception e) {
+        }
+    }
+
+    // Palauttaa P:n pienimmän avaimen. 
+    // Virhe esiintyy prioriteettijonon ollessa tyhjä.
+    public int minKey() {
+        try {
+
+        } catch (Exception e) {
+        }
+
+        return 0;
+    }
+
+    // Palauttaa P:n alkion, jolla on pienin avain.
+    // Virhe esiintyy prioriteettijonon ollessa tyhjä.
+    public void minElement() {
+
+    }
+
+    public void print() {
+
+    }
+
+    // Vaihtaa keon T solmujen v ja w alkiot keskenään.
+    // (Onko tarpeellinen?)
+    public void swap(HeapNode v, HeapNode w) {
+
+    }
+
+    // Korvaa solmun v alkion alkiolla e ja palauttaa aiemman alkion
+    // (Onko tarpeellinen?)
+    public HeapNode replace(HeapNode v, HeapNode e) {
+        return v;
+    }
+
+    // (Onko tarpeellinen?)
+    // (Onko tarpeellinen?)
+    // (Onko tarpeellinen?)
+    // (Onko tarpeellinen?)
+    // (Onko tarpeellinen?)    
 }
